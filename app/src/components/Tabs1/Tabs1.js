7b4a25103b79bf8b4e26fb18195272cec859ce74
Ramendra Namdev <ramendranamdev@gmail.com>
2020-11-10 23:13:55 +0530

    Tabs Added on home screen

---
+++ app/src/components/Tabs1/Tabs1.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Tab, TabHeading, Tabs, Icon} from 'native-base';
+import Camera from './Tabs/Camera';
+
+function Tabs1() {
+  return (
+    <Tabs transparent>
+      <Tab
+        heading={
+          <TabHeading>
+            <Icon type="FontAwesome" name="camera" style={{fontSize: 24}} />
+          </TabHeading>
+        }>
+        <Camera />
+      </Tab>
+
+      <Tab heading="Chat"></Tab>
+      <Tab heading="Status"></Tab>
+      <Tab heading="Call"></Tab>
+    </Tabs>
+  );
+}
+
+export default Tabs1;
